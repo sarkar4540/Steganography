22f9b1a5617fc43484f24762b9a6b52f5bf9c908
sarkar4540 <sarkar4540@gmail.com>
2017-06-10 13:34:50 +0530

    Fixed the green and blue replacement error!

--- src/steganography/StegoType1.java
+++ src/steganography/StegoType1.java
@@ -58,17 +58,17 @@ public class StegoType1 implements Stego{
                 if(red<0)red+=2;//checking out of bounds, if out of bounds, the value is changed keeping the lsb same.
                 if(red>255)red-=2;
                 
-                int blue=color>>8&0xFF;
-                blue=((blue%2==0)?blue:blue-1)+databit;//now for blue...
-                if(blue<0)blue+=2;
-                if(blue>255)blue-=2;
-                
-                int green=color&0xFF;
+                int green=color>>8&0xFF;
                 green=((green%2==0)?green:green-1)+databit;//now for green...
                 if(green<0)green+=2;
                 if(green>255)green-=2;
                 
-                color=(red<<16|green<<8|blue);
+                int blue=color&0xFF;
+                blue=((blue%2==0)?blue:blue-1)+databit;//now for blue...
+                if(blue<0)blue+=2;
+                if(blue>255)blue-=2;
+                
+                color=(red<<16|green<<8|blue);//Merging components
                 impl.setRGB(i, j, color);
             }
         }
